a4c68e8cd6b908a3189b555dfba3305eed895142
Furkan Fatih Arslan <arslanfurkanfatih@gmail.com>
2018-07-08 12:21:26 +0300

    microposts eklendi

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    has_many :microposts
+    validates :name, length: { maximum: 140 },
+                     presence: true 
+    validates :email, length: { maximum: 140 },
+    presence: true   
+  
 end
